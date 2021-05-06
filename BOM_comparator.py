f57f78dc0ddc6c41ff468df628774f3ced46f96c
g0lgoth <gautier.astoul@gmail.com>
2021-05-06 23:08:24 +0200

    good version

--- BOM_comparator.py
+++ BOM_comparator.py
@@ -1,60 +1,43 @@
 import os
 from openpyxl import load_workbook
 from openpyxl import Workbook
-from openpyxl import load_workbook
 
 excel_directory_move = os.getcwd()
 print(excel_directory_move)
+wb1 = load_workbook('{}\\E0000237-Auxiliary_Module_1-04_03_21.xlsx'.format(excel_directory_move))
+row_start = 13
+row_end = 15
+col = 3
 
-wb1 = load_workbook('{}\\E0000237-Auxiliary_Module_1-04_03_21.xls'.format(excel_directory_move))
-wb2 = load_workbook('{}\\BOM-E0000237-A01_initial.xlsx'.format(excel_directory_move))
-wb3 = Workbook()
-
-active_row = 13
-id_column = 6
-cursor_value = 0
-check_table = []
-
-class excelManagement():
+class ExcelManagement():
 
     def __init__(self, wb):
-        ws = self.wb.active
+        self.ws = wb.active
         
-    def get_value(self, row_cursor, col_cursor)
-        location = ws.cell(row=row_cursor, column=col_cursor)
-        cell_value= location.value
-        return (cell_value)
+    def get_value(self, row_cursor, col_cursor):
+        temp_location = self.ws.cell(row=row_cursor, column=col_cursor)
+        temp_cell_value= temp_location.value
+        return (temp_cell_value)
 
-def checkValue(wb, row_cursor, col_cursor):
-    ws = wb.active
-    location = ws.cell(row=row_cursor, column=col_cursor)
-    cell_value= location.value
-    return (cell_value)
+    def list_creator(self, row_start, row_end, col):
+        wb_list = []
+        for value in range (row_start, row_end):
+            wb_list.append(self.get_value(value, col))
+        return wb_list
 
-print(checkValue(wb1, active_row, id_column))
-value = checkValue(wb1, active_row, id_column)
-check_table.append(value)
-print(check_table)
-# for row in ws1.values:
-#     for value in row:
-#         print(value)
+class ListManagement():
 
-# wb2 = Workbook()
-# xlrd.open_workbook('{}\\BOM-E0000237-A01.xlsx'.format(excel_directory_move))
-# rb2 = xlrd.open_workbook('E0000237-Auxiliary_Modile_1-04_03_21.xlsx')
-# sheet1 = rb1.sheet_by_index(0)
-# sheet2 = rb2.sheet_by_index(0)
+    def __init__(self):
+        self.source_listA = ExcelManagement(wb1).list_creator(row_start,row_end,col)
+        self.source_listB = ExcelManagement(wb1).list_creator(row_start,row_end,col)
 
-# value_min = 13
+    def list_comparator_rank1(self, self.source_listA, self.source_listB):
+        destination_list = []
+        temp_list = []
+        for value_listA in self.source_listA:
+            for value_listB in self.source_listB:
+                if value_listA == value_listB:
+                    temp_list.append(self.get_value(value, col))
+        return wb_list
 
-# def maxRowValue(sheet):
-#     for col in sheet:
-#         for row in sheet:
-#             if row != None:
-#                 continue
-#             else:
-#                 return row.value
-
-# sheet1_max_value = maxRowValue(sheet1)
-# sheet2_max_value = maxRowValue(sheet2)
-# print(sheet1_max_value, sheet2_max_value)
+print(ExcelManagement(wb1).list_creator(13,15,3))
